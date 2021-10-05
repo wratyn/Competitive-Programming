988aa22cf4da5390a93c740cfb598f4331280e38
wratyn <91964690+wratyn@users.noreply.github.com>
2021-10-05 21:31:18 +0700

    Create b.cpp
    B - qwerty

---
+++ AtCoder/ABC218/b.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+#define ll long long
+ 
+int main(){
+	for(int i=0; i<26; i++){
+		int x;
+		cin >> x;
+		cout << (char)(x+96);
+	}
+	cout << endl;
+}
